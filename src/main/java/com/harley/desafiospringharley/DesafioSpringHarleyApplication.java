d1de19f67f88401c95e09ee970def4fe9f2c04c1
Harley de Brito <harley.brito@g.globo>
2022-07-25 14:55:35 -0300

    Primeiro commit

---
+++ src/main/java/com/harley/desafiospringharley/DesafioSpringHarleyApplication.java
@@ -0,0 +1,13 @@
+package com.harley.desafiospringharley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringHarleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringHarleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harley/desafiospringharley/DesafioSpringHarleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harley.desafiospringharley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringHarleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
